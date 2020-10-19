b7da5d06327a93da0dee1bc160d9fda50df32577
mell2262 <mellgomez.2262@gmail.com>
2020-10-19 11:26:18 +0200

    fase2

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,21 +12,12 @@ public class Restaurante {
 		luego escogeremos que queremos para comer. Una vez hecho esto se deberá calcular el precio de la comida el 
 		programa nos dirá con qué billetes debemos pagar.
 		
-		FASE 2
-		 Con un bucle for deberemos rellenar los dos arrays anteriormente creados. Añadiremos el nombre del plato y luego
-		 el precio. Puedes hacer uso de diccionarios de datos (Java HasMap)
-		 Una vez llenos los dos arrays tendremos que mostrarles y preguntar que se quiere para comer, guardaremos la 
-		información en una List usando un bucle while.
-		 Tendremos que preguntar si se quiere seguir pidiendo comida. Puede utilizar el sistema (1: Si / 0: No), por lo t
-		anto deberá crear otro variable int para guardar la información.
 		FASE 3
 		 Una vez hemos acabado de pedir la comida, tendremos que comparar la lista con el array que hemos hecho al 
 		principio. En caso de que la información que hemos introducido en la List coincida con la del array, tendremos 
 		que sumar el precio del producto solicitado; en el caso contrario tendremos que mostrar un mensaje que diga que 
 		el producto que hemos pedido no existe.
 
-		Recomendaciones: debes validar que los datos introducidos por consola cumplen los requerimientos de formato y
-		 extensión por medio del control de excepciones de Java.
 
 		 */ 
 		
@@ -46,7 +38,12 @@ public class Restaurante {
 		
 		for (int i=0; i<numMenu;i++) {
 			System.out.println("ingreso nombre y precio del plato "+i);
-			menu.put(scan.next(),scan.nextDouble());
+			try {
+				menu.put(scan.next(), scan.nextDouble());
+			} catch (InputMismatchException e) {
+				System.out.println("Ingrese un precio valido");
+				i--;
+			}
 		}
 		
 		for (String i : menu.keySet() ){
@@ -58,10 +55,19 @@ public class Restaurante {
 		while(more==1) {
 			System.out.println("¿Que quiere comer?");
 			order.add(scan.next());
-			System.out.println("quiere seguir pidiendo comida (1: Si / 0: No)");
-			more=scan.nextInt();
+			do {
+				System.out.println("quiere seguir pidiendo comida (1: Si / 0: No)");
+				try {
+					more=scan.nextInt();
+				} catch(InputMismatchException e) {
+				System.out.println("Ingrese 1: Si o 0: No");
+				}
+			
+			}while (more!=0 && more != 1);
+			}
+			
 		}
 
 	}
 
-}
+
